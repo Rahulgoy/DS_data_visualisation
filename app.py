cabb6a20655897078ddfac7d885b6770177e1c89
rahulgoy <rahulkumargoyal00@gmail.com>
2021-11-27 14:27:46 +0530

    Further Stats

--- app.py
+++ app.py
@@ -14,13 +14,13 @@ footer {visibility: hidden;}
 </style> """, unsafe_allow_html=True)
 
 data = pd.read_csv("googleplaystore.csv")
-
+data = data.dropna()
 
 # Header
 st.header("Google Play Store Data Visualization")
 
 
-# Metrices
+# 1. Metrices
 categories = data['Category'].unique()
 col01, col1, col2, col02= st.columns(4)
 col1.metric("Total Apps", len(data))
@@ -28,29 +28,44 @@ col2.metric("Total Categories",len(categories))
 # col3.metric("Humidity", "86%", "4%")
 
 
+# 2. Show Dataset
 agree = st.checkbox('See the dataset!')
 if agree:
     st.dataframe(data)
 
 
-labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
-sizes = [15, 30, 45, 10]
-# explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
-
+#
 fig1, ax1 = plt.subplots()
 ax1.pie(data['Category'].value_counts(), labels=categories, autopct='%1.1f%%',
         shadow=True, startangle=90)
 ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 
 # st.pyplot(fig1)
+
+# 3. Category wise data distri
+st.subheader("Category wise data distribution")
 st.bar_chart(data['Category'].value_counts(), height=400)
 
 
+# 4. Content Rating
+content_categories = data['Content Rating'].unique()
+dataf = data['Content Rating'].value_counts()
+# st.write(data['Content Rating'].value_counts())
+st.subheader("Content Ratings")
+fig = px.pie(dataf, values="Content Rating", names=content_categories)
+fig.update_layout(margin=dict(l=1,r=1,b=1,t=1),paper_bgcolor="white", height=350, width=500, font_color="black")
+st.write(fig)
+
+
+# 4.   Categorie wise rating
+result = data.groupby(['Category']).mean()
+
+st.subheader("Category wise average rating")
+st.bar_chart(result['Rating'],height=350,use_container_width=True)
 # To implement correlation 
 # fig, ax = plt.subplots()
 # sns.heatmap(data.corr(), ax=ax)
 # st.write(fig)
-
 
 
 st.sidebar.header("Please Filter Here:")
@@ -60,3 +75,17 @@ st.sidebar.header("Please Filter Here:")
 #     options=data["App"].unique
 # )
 st.sidebar.write("Made with ❤ by Rahul Goyal")
+
+
+# Top 10 by categories with highest ratings
+
+cat = st.multiselect("Select the categorie",options=categories)
+datafra = data.query("Category == @cat")
+datafra = datafra.sort_values(by=['Rating'], ascending=False)
+datafra = datafra.iloc[:10]
+datafra = datafra[['App','Rating']]
+cat_fig = px.bar(datafra,x="Rating",y="App",orientation='h')
+cat_fig.update_layout(margin=dict(l=1,r=2,b=1,t=2),height=500, width=700)
+
+st.write(cat_fig)
+
