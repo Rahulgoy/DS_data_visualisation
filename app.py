aa919762e1887f090ea95ed5dd65791f6dae16ad
rahulgoy <rahulkumargoyal00@gmail.com>
2021-11-30 00:05:32 +0530

    Top 10

--- app.py
+++ app.py
@@ -45,16 +45,25 @@ ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 # 3. Category wise data distri
 st.subheader("Category wise data distribution")
 st.bar_chart(data['Category'].value_counts(), height=400)
+# bar_fig = px.bar(data['Category'].value_counts())
+# st.write(bar_fig)
 
 
-# 4. Content Rating
-content_categories = data['Content Rating'].unique()
-dataf = data['Content Rating'].value_counts()
-# st.write(data['Content Rating'].value_counts())
-st.subheader("Content Ratings")
-fig = px.pie(dataf, values="Content Rating", names=content_categories)
-fig.update_layout(margin=dict(l=1,r=1,b=1,t=1),paper_bgcolor="white", height=350, width=500, font_color="black")
-st.write(fig)
+center_col1,center_col2 = st.columns(2)
+with center_col1:
+    # 4. Content Rating
+    content_categories = data['Content Rating'].unique()
+    dataf = data['Content Rating'].value_counts()
+    # st.write(data['Content Rating'].value_counts())
+    st.subheader("Content Ratings")
+    fig = px.pie(dataf, values="Content Rating", names=content_categories)
+    fig.update_layout(margin=dict(l=1,r=1,b=1,t=1),paper_bgcolor="white", height=350, width=500, font_color="black")
+    st.write(fig)
+with center_col2:
+    st.subheader("Rating wise App Count")
+    group = data.groupby(['Rating']).count()
+    # st.dataframe(group)
+    st.line_chart(group['App'],use_container_width=False,width=500,height=350)
 
 
 # 4.   Categorie wise rating
@@ -79,13 +88,29 @@ st.sidebar.write("Made with ❤ by Rahul Goyal")
 
 # Top 10 by categories with highest ratings
 
-cat = st.multiselect("Select the categorie",options=categories)
-datafra = data.query("Category == @cat")
-datafra = datafra.sort_values(by=['Rating'], ascending=False)
-datafra = datafra.iloc[:10]
-datafra = datafra[['App','Rating']]
-cat_fig = px.bar(datafra,x="Rating",y="App",orientation='h')
-cat_fig.update_layout(margin=dict(l=1,r=2,b=1,t=2),height=500, width=700)
+cat = st.selectbox("Select the categorie",options=categories)
+# st.write(datafra)
+# cat_fig = px.bar(datafra,x="Rating",y="App",orientation='h')
+# cat_fig.update_layout(margin=dict(l=1,r=2,b=1,t=2),height=500, width=700)
 
-st.write(cat_fig)
+# st.write(cat_fig)
+
+
+cat_col1,cat_col2 = st.columns(2)
+
+with cat_col1:
+    st.subheader("Top 10 highest rated Apps by Categorie")
+    datafra = data.query("Category == @cat")
+    datafra = datafra.sort_values(by=['Rating'], ascending=False)
+    datafra = datafra.iloc[:10]
+    datafra = datafra[['App','Rating']]
+    st.table(datafra)
+with cat_col2:
+    st.subheader("Top 10 Free Apps by Categorie")
+    datafra = data.query("Category == @cat and Type == 'Free'")
+    datafra = datafra.sort_values(by=['Rating'], ascending=False)
+    datafra = datafra.iloc[:10]
+    datafra = datafra[['App','Rating']]
+    st.table(datafra)
+
 
